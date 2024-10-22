96e7306b47b30f5a24459a967b2d7e7872360622
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-10-22 15:47:15 +0300

    validation functions

--- src/app/input-validator/input-validator.component.ts
+++ src/app/input-validator/input-validator.component.ts
@@ -10,4 +10,12 @@ import {AbstractControl, FormControl} from "@angular/forms";
 export class InputValidatorComponent {
   @Input() property: AbstractControl | null = new FormControl('');
   @Input() minLength: number = 0;
+
+  get isInvalid(): boolean {
+    return this.property?.invalid && this.property?.touched || false;
+  }
+
+  get hasMinLengthError(): boolean {
+    return this.property?.hasError('minlength') && this.minLength > 0 || false;
+  }
 }
