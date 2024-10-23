dee33faa405b3e05b0702115790d2de508b16ab1
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-10-23 12:18:51 +0300

    route to task-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from "@angular/router";
 import {CreateTaskComponent} from "./features/create-task/create-task.component";
 import {ErrorPageComponent} from "./features/error-page/error-page.component";
+import {TaskListComponent} from "./features/task-list/task-list.component";
 
 export const routes: Routes = [
   {path: 'create-task', title: 'TODO - Create', component: CreateTaskComponent},
+  {path: 'task-list', title: 'TODO - List', component: TaskListComponent},
   { path: '',   redirectTo: '/create-task', pathMatch: 'full' },
   {path: '**', title: 'TODO - Error', component: ErrorPageComponent}
 ];
