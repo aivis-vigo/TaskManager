8ddeeae9988dfd895e6a383250e56afbd3e7e831
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-10-23 14:58:28 +0300

    lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import {Routes} from "@angular/router";
-import {CreateTaskComponent} from "./features/create-task/create-task.component";
-import {ErrorPageComponent} from "./features/error-page/error-page.component";
-import {TaskListComponent} from "./features/task-list/task-list.component";
 
 export const routes: Routes = [
-  {path: 'create-task', title: 'TODO - Create', component: CreateTaskComponent},
-  {path: 'task-list', title: 'TODO - List', component: TaskListComponent},
-  { path: '',   redirectTo: '/create-task', pathMatch: 'full' },
-  {path: '**', title: 'TODO - Error', component: ErrorPageComponent}
+  {
+    path: 'create-task',
+    title: 'TODO - Create',
+    loadComponent: () => import('./features/create-task/create-task.component').then(c => c.CreateTaskComponent)
+  },
+  {
+    path: 'task-list',
+    title: 'TODO - List',
+    loadComponent: () => import('./features/task-list/task-list.component').then(c => c.TaskListComponent)
+  },
+  {path: '', redirectTo: '/create-task', pathMatch: 'full'},
+  {
+    path: '**',
+    title: 'TODO - Error',
+    loadComponent: () => import('./features/error-page/error-page.component').then(c => c.ErrorPageComponent)
+  }
 ];
