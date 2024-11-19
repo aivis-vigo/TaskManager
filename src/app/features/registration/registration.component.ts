b4fe76b6fa331dae4f5f94f9a26ac791240c9618
aivis-vigo <aivisvigoreimarts@gmail.com>
2024-11-19 09:15:12 +0200

    not needed

--- src/app/features/registration/registration.component.ts
+++ src/app/features/registration/registration.component.ts
@@ -30,7 +30,6 @@ export class RegistrationComponent implements OnDestroy {
     lastName: ['', [Validators.required]],
     username: ['', [Validators.required]],
     password: ['', [Validators.required]],
-    role: [['User'], [Validators.required]],
     confirmPassword: ['', [
       Validators.required,
       matchingPasswordsValidator('password')
